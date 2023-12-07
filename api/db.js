3de686291fc0078d66761c3f9335fa3d75df5ad8
Vaibhav Thapliyal <Vaibhav.Thapliyal@iiitb.ac.in>
2023-12-08 03:26:31 +0530

    sab kuch jala denge 4

--- api/db.js
+++ api/db.js
@@ -1,9 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password: "Pass_word1",
-  database:"blog",
-  port: 3306,
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "Pass_word1",
+  database: process.env.MYSQL_DATABASE || "blog",
 })
