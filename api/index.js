66e8c223d386c0f919c0e170dacf8d805e59ebc3
Vaibhav Thapliyal <Vaibhav.Thapliyal@iiitb.ac.in>
2023-12-08 01:35:19 +0530

    edited docker compose 6

--- api/index.js
+++ api/index.js
@@ -29,6 +29,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(8800, () => {
+app.listen(8000, () => {
   console.log("Connected!");
 });
