8753ec0670077bcd269fee9f7d548dc7e68760cf
Vaibhav Thapliyal <Vaibhav.Thapliyal@iiitb.ac.in>
2023-12-09 16:04:02 +0530

    sksks 3

--- api/index.js
+++ api/index.js
@@ -25,9 +25,9 @@ app.post("/api/upload", upload.single("file"), function (req, res) {
   res.status(200).json(file.filename);
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 app.listen(8000, () => {
   console.log("Connected!");
