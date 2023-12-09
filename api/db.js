837ef1a1ccc253069c7d639b947c037a05f98e5f
Vaibhav Thapliyal <Vaibhav.Thapliyal@iiitb.ac.in>
2023-12-09 20:59:03 +0530

    project change

--- api/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql"
-
-export const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST || "localhost",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "Pass_word1",
-  database: process.env.MYSQL_DATABASE || "blog",
-  // port: 3307,
-})
